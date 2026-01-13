e2f2eab395dd58bebe31764944e431c403bb6234
Vinícius Malheiros Santos <viniciusmalheirossantos@MacBook-Pro-de-Vinicius.local>
2026-01-13 12:17:44 -0300

    Update PRICE_ID for production

--- api/checkout.js
+++ api/checkout.js
@@ -11,7 +11,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: '2023-10-16',
 });
 
-const PRICE_ID = 'price_1SjPMdA4LY5asU1J4urTPUPp';
+const PRICE_ID = 'price_1SnXUmA6WnMHKTVrtOuxsfPe';
 const TRIAL_DAYS = 1;
 const MAX_TRIALS_PER_IP = 3;
 
